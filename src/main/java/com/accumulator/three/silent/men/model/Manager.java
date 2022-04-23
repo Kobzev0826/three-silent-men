ba76ae8f50c9a229d054f87303fa40b013d00d0f
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2022-04-23 23:59:53 +0300

    мои попытки чтения файла конфигурации #2

--- src/main/java/com/accumulator/three/silent/men/model/Manager.java
+++ src/main/java/com/accumulator/three/silent/men/model/Manager.java
@@ -1,6 +1,12 @@
 package com.accumulator.three.silent.men.model;
 
+import com.accumulator.three.silent.men.DatabaseConnection;
+import com.accumulator.three.silent.men.DatabaseParser;
+import com.accumulator.three.silent.men.MapFilter;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +14,7 @@ import static com.accumulator.three.silent.men.fileread.CnfigYml.Yml_parse;
 
 public class Manager {
 
-    public void start(String ConfigDataPath) throws FileNotFoundException {
+    public void start(String ConfigDataPath) throws IOException, URISyntaxException {
 
         Map<String, Object> data  = Yml_parse(ConfigDataPath);
         List<String> validAttributes = (List<String>) data.get("validAttributes");
@@ -19,8 +25,9 @@ public class Manager {
         for (int i=1; i<data.size();i++){
             String name = "source" + i;
             Map<String, Object> source = (Map<String, Object>) data.get(name);
-
-
+            List<Map<String, Object>> ParseData = SwichParser.Swich(source);
+            ParseData = (List<Map<String, Object>>) MapFilter.filterMap((Map<String, Object>) ParseData,validAttributes);
+            DatabaseConnection.writeToDatabaseMany(ParseData);
 
         }
     }
--- src/main/java/com/accumulator/three/silent/men/model/SwichParser.java
+++ src/main/java/com/accumulator/three/silent/men/model/SwichParser.java
@@ -4,13 +4,12 @@ import com.accumulator.three.silent.men.fileread.CSVParser;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SwichParser {
 
-    public void Swich(Map<String, Object>  SourcePar) throws IOException, URISyntaxException {
+    public static List<Map<String, Object>> Swich(Map<String, Object> SourcePar) throws IOException, URISyntaxException {
         List<Map<String, Object>> ParseData;
         switch (SourcePar.get("type")) {
 
